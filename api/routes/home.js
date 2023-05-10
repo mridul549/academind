a7106ec9ee291adf3c3df0750247d787f92d5558
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-05-10 10:27:19 +0530

    secondcommit

--- api/routes/home.js
+++ api/routes/home.js
@@ -4,4 +4,11 @@ const router = express.Router();
 router.use('/products', require("./products"));
 router.use('/orders', require('./orders'));
 
+router.get('/', (req,res)=> {
+    res.status(200).json({
+        message: "Sanyam chutiya hai, Mridul is the best",
+        id: 1
+    })
+})
+
 module.exports = router;
